476b3822b6e71039d5c5e3974f08e8e3fa6d71be
overallcoma <30637170+overallcoma@users.noreply.github.com>
2018-09-06 09:55:04 -0500

    Working Dialogue Replacements

--- CTF-PhaserQuest.py
+++ CTF-PhaserQuest.py
@@ -1,2 +1,33 @@
 from configparser import ConfigParser
+import json
+import re
 
+configfilename = "./CTF-PhaserQuest.cfg"
+configinfo = ConfigParser()
+configinfo.read(configfilename)
+try:
+    configinfo.read(configfilename)
+except:
+    print("Unable to read config file")
+    exit(1)
+
+phaserquestdb = "./assets/json/db.json"
+phaserquestdb_json = open(phaserquestdb, "r")
+phaserquestdb_json = json.load(phaserquestdb_json)
+
+
+def modify_npc_diaglogue(json_data, character_name, dialogue):
+    dialogue_items = re.findall(r'\"(.+?)\"', dialogue)
+    json_data["npc"][character_name]["dialogue"] = dialogue_items
+
+
+def save_modified_json(json_data, outputfile):
+    outputfile = open(outputfile, "w+")
+    json.dump(json_data, outputfile, sort_keys=True, indent=4)
+
+npc_dialogues = configinfo["npc-dialogue"]
+for entry in npc_dialogues:
+    dialogue_to_insert = npc_dialogues[entry]
+    modify_npc_diaglogue(phaserquestdb_json, entry, dialogue_to_insert)
+
+save_modified_json(phaserquestdb_json, phaserquestdb)
